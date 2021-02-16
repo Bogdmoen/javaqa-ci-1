86f44b58b304ecade1016c7f25682641d0e7343c
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-02-16 04:25:42 +0300

    service updated

--- src/test/java/ru/netology/statistics/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistics/StatisticsServiceTest.java
@@ -9,22 +9,22 @@ class StatisticsServiceTest {
 
 
     @Test
-    void shouldFindMaxIndexFirstInArray() {
+    void shouldFindMax() {
         long[] incomesInBillions = {12, 5, 8, 4, 5, 11, 8, 6, 11, 11, 12};
-        long expected = 0;
+        long expected = 12;
 
-        long actual = service.findMaxIndex(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
 
     }
 
     @Test
-    void shouldFindMaxIndexMiddleInArray() {
+    void shouldFindMaxMiddleInArray() {
         long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 30, 12};
-        long expected = 5;
+        long expected = 30;
 
-        long actual = service.findMaxIndex(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
 
@@ -33,9 +33,9 @@ class StatisticsServiceTest {
     @Test
     void shouldFindMaxIndexEndInArray() {
         long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 40};
-        long expected = 10;
+        long expected = 40;
 
-        long actual = service.findMaxIndex(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
 
